cd9248555af03d4ae356d8e4d57aff049728175d
Secan A. <dariusthegiant@gmail.com>
2022-10-16 23:14:00 +0200

    add 90 day heatmap and weekly/monthly averages

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # mood-log
 
-a tiny page to log my mood each day. made this on a quiet weekend while learning js, partly because i kept forgetting how the week actually felt by friday.
+a tiny page to log my mood each day and see a heatmap of the last few months. made this on a quiet weekend while learning js, partly because i kept forgetting how the week actually felt by friday.
 
 ## what it does
 
 - pick a mood from 5 levels: great, good, ok, low, rough. each level has its own color swatch.
 - one entry per day. if you change your mind you can just click again and it overwrites today.
 - everything stored in localStorage, no backend.
+- shows a 90 day calendar heatmap where each cell is colored by that day's mood.
+- shows the average mood for the last 7 and last 30 days.
+
+## the math bit
+
+mood values are scaled 1 to 5. average is just the plain mean of however many days are filled in that window. heatmap colors are a linear interpolation between two color stops based on the mood value, so a 3 lands halfway between the low and high color.
 
 ## how to run it
 
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // mood log
 // 5 levels, one entry per day, kept in localStorage.
+// today's entry can be overwritten if you change your mind.
 
 const MOODS = [
   { value: 5, key: "great", label: "great", color: "#f7c66b" },
@@ -10,6 +11,9 @@ const MOODS = [
 ];
 
 const STORAGE_KEY = "mood-log.entries.v1";
+// linear interpolate heatmap stops, low to high
+const STOP_LOW  = [107, 122, 143]; // #6b7a8f
+const STOP_HIGH = [247, 198, 107]; // #f7c66b
 
 function loadEntries() {
   try {
@@ -23,12 +27,30 @@ function saveEntries(entries) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(entries));
 }
 
-function todayKey() {
-  const d = new Date();
+function dateKey(d) {
   const y = d.getFullYear();
   const m = String(d.getMonth() + 1).padStart(2, "0");
   const day = String(d.getDate()).padStart(2, "0");
   return `${y}-${m}-${day}`;
+}
+
+function todayKey() {
+  return dateKey(new Date());
+}
+
+function colorForMood(value) {
+  // value is 1..5, map to 0..1 then lerp the two stops
+  const t = (value - 1) / 4;
+  const r = Math.round(STOP_LOW[0] + (STOP_HIGH[0] - STOP_LOW[0]) * t);
+  const g = Math.round(STOP_LOW[1] + (STOP_HIGH[1] - STOP_LOW[1]) * t);
+  const b = Math.round(STOP_LOW[2] + (STOP_HIGH[2] - STOP_LOW[2]) * t);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function average(values) {
+  if (!values.length) return null;
+  const sum = values.reduce((a, b) => a + b, 0);
+  return sum / values.length;
 }
 
 function buildPicker(entries) {
@@ -45,6 +67,8 @@ function buildPicker(entries) {
       entries[today] = m.value;
       saveEntries(entries);
       buildPicker(entries);
+      buildHeatmap(entries);
+      updateAverages(entries);
       setStatus(`saved ${m.label} for today`);
     });
     wrap.appendChild(btn);
@@ -55,9 +79,54 @@ function setStatus(text) {
   document.getElementById("status").textContent = text;
 }
 
+function buildHeatmap(entries) {
+  const wrap = document.getElementById("heatmap");
+  wrap.innerHTML = "";
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  // 90 cells, oldest first so today ends up bottom-right
+  for (let i = 89; i >= 0; i--) {
+    const d = new Date(today);
+    d.setDate(today.getDate() - i);
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    const k = dateKey(d);
+    const v = entries[k];
+    if (v) {
+      cell.style.background = colorForMood(v);
+      cell.title = `${k}: ${MOODS.find(m => m.value === v).label}`;
+    } else {
+      cell.title = `${k}: no entry`;
+    }
+    wrap.appendChild(cell);
+  }
+}
+
+function updateAverages(entries) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const week = [];
+  const month = [];
+  for (let i = 0; i < 30; i++) {
+    const d = new Date(today);
+    d.setDate(today.getDate() - i);
+    const v = entries[dateKey(d)];
+    if (v) {
+      month.push(v);
+      if (i < 7) week.push(v);
+    }
+  }
+  const w = average(week);
+  const m = average(month);
+  document.getElementById("avg-week").textContent = w === null ? "-" : w.toFixed(2);
+  document.getElementById("avg-month").textContent = m === null ? "-" : m.toFixed(2);
+}
+
 function init() {
   const entries = loadEntries();
   buildPicker(entries);
+  buildHeatmap(entries);
+  updateAverages(entries);
 }
 
 init();
